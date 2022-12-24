445f7d6579f913f1da9529f87d309b5f1bc215da
Paul-Erwan RIO <paulerwan.rio@gmail.com>
2022-12-24 19:43:36 +0100

    puzzle: split in modules

---
+++ src/puzzle/corner.rs
@@ -0,0 +1,92 @@
+//
+// This file is part of flash-img-seeker
+//
+// Copyright (C) 2022 Paul-Erwan RIO <paulerwan.rio@proton.me>
+//
+//
+use crate::error::*;
+use crate::puzzle::parray::slot::SlotStatus;
+use ndarray::{ArrayBase, Ix1, Ix2, ViewRepr};
+
+#[derive(Debug)]
+pub struct Corner {
+    // 'corner' unicode character to draw
+    pub c: char,
+    // clockwise (starting from top) list of edges that characterize the corner
+    split: [bool; 4],
+}
+
+impl Corner {
+    pub fn new(c: char) -> Self {
+        Self {
+            c,
+            split: [false, false, false, false],
+        }
+    }
+
+    pub fn set_top(mut self) -> Self {
+        self.split[0] = true;
+        self
+    }
+
+    pub fn set_right(mut self) -> Self {
+        self.split[1] = true;
+        self
+    }
+
+    pub fn set_bottom(mut self) -> Self {
+        self.split[2] = true;
+        self
+    }
+
+    pub fn set_left(mut self) -> Self {
+        self.split[3] = true;
+        self
+    }
+
+    /// Check if the Corner can be used in this case, based on the list of SlotStatus around the corner.
+    pub fn is_usable_for(&self, slots: ClockWiseSlots) -> bool {
+        let template: [bool; 4] = [
+            slots.inner[0] != slots.inner[1],
+            slots.inner[1] != slots.inner[2],
+            slots.inner[2] != slots.inner[3],
+            slots.inner[3] != slots.inner[0],
+        ];
+        self.split.iter().zip(template.iter()).all(|(a, b)| a == b)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct ClockWiseSlots {
+    pub inner: [SlotStatus; 4],
+}
+
+impl TryFrom<ArrayBase<ViewRepr<&SlotStatus>, Ix2>> for ClockWiseSlots {
+    type Error = Error;
+
+    fn try_from(value: ArrayBase<ViewRepr<&SlotStatus>, Ix2>) -> Result<Self> {
+        if value.dim() != (2, 2) {
+            Err(Error::BadShape)
+        } else {
+            Ok(Self {
+                inner: [value[[0, 0]], value[[0, 1]], value[[1, 1]], value[[1, 0]]],
+            })
+        }
+    }
+}
+
+type Custom<'a> = ArrayBase<ViewRepr<&'a SlotStatus>, Ix1>;
+
+impl TryFrom<(Custom<'_>, Custom<'_>)> for ClockWiseSlots {
+    type Error = Error;
+
+    fn try_from(value: (Custom<'_>, Custom<'_>)) -> Result<Self> {
+        if value.0.dim() != (2) || value.1.dim() != (2) {
+            Err(Error::BadShape)
+        } else {
+            Ok(Self {
+                inner: [value.0[[0]], value.1[[0]], value.1[[1]], value.0[[1]]],
+            })
+        }
+    }
+}
--- src/puzzle/mod.rs
+++ src/puzzle/mod.rs
@@ -4,11 +4,14 @@
 // Copyright (C) 2022 Paul-Erwan RIO <paulerwan.rio@proton.me>
 //
 //
+mod corner;
+mod dynzip;
 mod parray;
 
 use crate::error::*;
 use colored::Colorize;
-use ndarray::{ArrayBase, Ix1, Ix2, ViewRepr};
+use corner::{ClockWiseSlots, Corner};
+use dynzip::DynamicZip;
 use parray::{slot::SlotStatus, PieceArray};
 use std::fmt;
 use term_size;
@@ -39,108 +42,6 @@ impl PuzzlePiece {
 
     fn name(&self) -> &str {
         self.bin_name.as_str()
-    }
-}
-
-#[derive(Debug)]
-struct Corner {
-    // 'corner' unicode character to draw
-    c: char,
-    // clockwise (starting from top) list of edges that characterize the corner
-    split: [bool; 4],
-}
-
-impl Corner {
-    fn new(c: char) -> Self {
-        Self {
-            c,
-            split: [false, false, false, false],
-        }
-    }
-
-    fn set_top(mut self) -> Self {
-        self.split[0] = true;
-        self
-    }
-
-    fn set_right(mut self) -> Self {
-        self.split[1] = true;
-        self
-    }
-
-    fn set_bottom(mut self) -> Self {
-        self.split[2] = true;
-        self
-    }
-
-    fn set_left(mut self) -> Self {
-        self.split[3] = true;
-        self
-    }
-
-    /// Check if the Corner can be used in this case, based on the list of SlotStatus around the corner.
-    fn is_usable_for(&self, slots: ClockWiseSlots) -> bool {
-        let template: [bool; 4] = [
-            slots.inner[0] != slots.inner[1],
-            slots.inner[1] != slots.inner[2],
-            slots.inner[2] != slots.inner[3],
-            slots.inner[3] != slots.inner[0],
-        ];
-        self.split.iter().zip(template.iter()).all(|(a, b)| a == b)
-    }
-}
-
-#[derive(Clone, Copy)]
-struct ClockWiseSlots {
-    inner: [SlotStatus; 4],
-}
-
-impl TryFrom<ArrayBase<ViewRepr<&SlotStatus>, Ix2>> for ClockWiseSlots {
-    type Error = Error;
-
-    fn try_from(value: ArrayBase<ViewRepr<&SlotStatus>, Ix2>) -> Result<Self> {
-        if value.dim() != (2, 2) {
-            Err(Error::BadShape)
-        } else {
-            Ok(Self {
-                inner: [value[[0, 0]], value[[0, 1]], value[[1, 1]], value[[1, 0]]],
-            })
-        }
-    }
-}
-
-type Custom<'a> = ArrayBase<ViewRepr<&'a SlotStatus>, Ix1>;
-
-impl TryFrom<(Custom<'_>, Custom<'_>)> for ClockWiseSlots {
-    type Error = Error;
-
-    fn try_from(value: (Custom<'_>, Custom<'_>)) -> Result<Self> {
-        if value.0.dim() != (2) || value.1.dim() != (2) {
-            Err(Error::BadShape)
-        } else {
-            Ok(Self {
-                inner: [value.0[[0]], value.1[[0]], value.1[[1]], value.0[[1]]],
-            })
-        }
-    }
-}
-
-// https://stackoverflow.com/questions/54756166/how-do-i-efficiently-iterate-through-a-vecvect-row-by-row
-struct DynamicZip<I>
-where
-    I: Iterator,
-{
-    iterators: Vec<I>,
-}
-
-impl<I, T> Iterator for DynamicZip<I>
-where
-    I: Iterator<Item = T>,
-{
-    type Item = Vec<T>;
-    fn next(&mut self) -> Option<Self::Item> {
-        let output: Option<Vec<T>> = self.iterators.iter_mut().map(|iter| iter.next()).collect();
-        output
     }
 }
 
